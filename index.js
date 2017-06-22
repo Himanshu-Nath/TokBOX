e1e24063302da78391d5dd327484ec69710ff0a1
Himanshu Nath <hnath723@gmail.com>
2017-06-22 13:01:41 +0530

    Change server from http to https

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var consts = require("./server/const/const.js")
 var express = require('express');
+var https = require('https');
 var bodyparser = require('body-parser');
+var fs = require('fs');
 var app = express();
 var sessionCURD = require("./server/route/sessionCURD.js")
+
+var options = {  
+  key: fs.readFileSync('keys/hostkey.pem'),  
+  cert: fs.readFileSync('keys/hostcert.pem')  
+};  
+
+console.log(options)
 
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
@@ -15,6 +24,13 @@ app.get('/', function (req, res) {
    res.sendFile( __dirname + "/index.html");
 })
 
-app.listen(consts.port, function() {
-    console.log("server started on port: "+consts.port);
-});
+// app.listen(consts.port, function() {
+//     console.log("server started on port: "+consts.port);
+// });
+
+// https.createServer(options, function (req, res) {  
+//  res.writeHead(200);  
+//   res.end("hello world\n");  
+// }).listen(3002); 
+
+https.createServer(options, app).listen(consts.port);
